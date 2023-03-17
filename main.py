7da631aa4392aaf3f5dfd815be478681c56fecd2
AlbertM <albertmunoz131@gmail.com>
2023-03-17 16:05:15 -0500

    Agregué el esquema del usuario y realicé un endpoint para agregar al usuario a la base de datos

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -46,3 +46,20 @@ def listar_justOne(id_usuario:int):
         if user['id'] == id_usuario:
             return user
     return {"message": "No se encontro el usuario en la base de datos"}
+
+class User(BaseModel):
+    username: str
+    id:int
+    password:str
+
+@app.post("/user/addusr/")
+def add_usr(user:User):
+    #id username password
+
+    for usuario in usuarios:
+        if user.username == usuario['username']:
+            return {"message": "Usuario ya existe"}
+    
+    #metodo para agregar en la base de datos
+    usuarios.append(user)
+    return {"message": "Usuario añadido con exito"}
